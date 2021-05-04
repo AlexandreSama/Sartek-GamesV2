8027785bdbe8bc3d5f6e0118ef4ad1e65a3cdf8e
Patouuu <serveur.khg.fr@outlook.fr>
2021-05-04 17:30:39 +0200

    maj serverinfo

--- Command/serverinfo.js
+++ Command/serverinfo.js
@@ -8,12 +8,14 @@ module.exports.run = (client, message) => {
 
     const embed = new Discord.MessageEmbed()
     .setTitle(`Infos du serveur ${guild.name}`)
-    .setAuthor(message.author.username)
+    .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic : true}))
     .addField("Nombres d'utilisateurs", `${guild.memberCount}`, true)
+    .addField("Nombres de channels", `${guild.channels.cache.size}`, true)
     .addField("Propriétaire du serveur", `<@${guild.ownerID}>`, true)
     .addField("Role le plus haut", `${guild.roles.highest}`, true)
     .addField("Région du serveur", `${guild.region}`, true)
-    .setFooter(guild.id)
+    .addField('Date de création du serveur', `${guild.createdAt}`, true)
+    .setFooter(`Id du serveur: ${guild.id}`)
 
     message.channel.send(embed)
 
