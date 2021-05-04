44537f166ff390faa299eb8bb15d70c98663120b
Alexandre <alexandrehainy12@gmail.com>
2021-05-04 18:46:46 +0200

    add setTimeout

--- Command/clear.js
+++ Command/clear.js
@@ -7,9 +7,9 @@ module.exports.run = (client, message, args) => {
 	if (message.member.hasPermission("ADMINISTRATOR")) {
         let arguments = args.join("")
         if(!arguments){
-            message.channel.send("Tu dois me donner un nombre de messages a supprimer stp !").then(messsages => messsages.delete())
+            message.channel.send("Tu dois me donner un nombre de messages a supprimer stp !").then(messsages => setTimeout(messsages.delete(), 60000))
         }else{
-            message.channel.bulkDelete(arguments).then(messages => message.channel.send(arguments + " messages supprimés").then(messsages => messsages.delete()))
+            message.channel.bulkDelete(arguments).then(messages => message.channel.send(arguments + " messages supprimés").then(messsages => setTimeout(messsages.delete(), 60000)))
         }
     } else {
         message.author.send("Tu n'a pas la permission de faire cet commande !")
