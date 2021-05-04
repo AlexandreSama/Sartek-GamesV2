2266dc6ae3aef8f475861b8da9fdf7d54e2c90d1
Alexandre <alexandrehainy12@gmail.com>
2021-05-04 17:52:43 +0200

    finish broadcast cmd

---
+++ Command/broadcast.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, message, args) => {
+
+    message.delete()
+
+    if(message.author.id === "724693796499095552" || message.author.id === "256892994504884224"){
+        client.guilds.cache.each(guild => {
+            try {
+                let channel = guild.channels.cache.find(channel => channel.name === "maj-patouuu-bot")
+                if(channel){
+                    channel.send(args)
+                } else{
+                    guild.channels.create("maj-patouuu-bot", {
+                        type: "text",
+                        topic: "Channel ou est noté chaque mise a jour du bot !"
+                    }).then(() => {
+                        let channel = guild.channels.cache.find(channel => channel.name === "maj-patouuu-bot")
+                        channel.send(args)
+                    })
+                }
+            } catch (err) {
+                console.log("Impossible d'envoyer un message dans le serveur " + guild.name)
+            }
+        });
+    }
+
+}
+
+module.exports.help = {
+    name: 'broadcast'
+};
+
