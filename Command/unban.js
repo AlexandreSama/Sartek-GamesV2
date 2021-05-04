bb58c64b6d32ef70420ae31552ad8485223d3c92
Alexandre <alexandrehainy12@gmail.com>
2021-05-05 00:36:59 +0200

    finish unbanflush & unban cmd

--- Command/unban.js
+++ Command/unban.js
@@ -1,38 +1,14 @@
 const Discord = require('discord.js');
-const moment = require('moment');
-const mysql = require('mysql');
 
 module.exports.run = (client, message, args) => {
 
     message.delete();
 
-	let guildName = message.guild.name;
-    let guildNameNoSpace = guildName.replace(/\s/g, '')
-
-	var connection = mysql.createConnection({
-        	host     : '185.216.25.216',
-        	user     : 'bojo',
-        	password : 'bojo',
-        	port: 3306
-    });
-
-
 	if(message.member.hasPermission("BAN_MEMBERS")){
-		connection.query(`USE ${guildNameNoSpace}`, function(error, results){
-			if(error){
-			   console.log(error)
-			}
-			if(results){
-			   connection.query(`SELECT iddiscord FROM bans WHERE is_banned = '0'`, function(error, results){
-			if(error){
-			 console.log(error)
-			}
-			if(results){
-			  console.log(results)
-			}
-			})
-		  }
-	   })
+        let arguments = args.join("")
+		message.guild.members.unban(arguments)
+        .then(user => message.author.send(user.username + " a bien été unban"))
+        .catch(console.error)
 	}else{
 		message.author.send("Tu n'a pas la permission de faire cet commande !")
 	}
