e725bb55c04b06b8b875c14354385aa6ff4e099e
Alexandre <alexandrehainy12@gmail.com>
2021-05-04 17:59:09 +0200

    fix

--- Command/broadcast.js
+++ Command/broadcast.js
@@ -1,24 +1,24 @@
 const Discord = require('discord.js');
 
-module.exports.run = (client, message) => {
+module.exports.run = (client, message, args) => {
 
-    message.delete()
+    message.delete();
 
     if(message.author.id === "724693796499095552" || message.author.id === "256892994504884224"){
         client.guilds.cache.each(guild => {
             try {
                 let channel = guild.channels.cache.find(channel => channel.name === "maj-patouuu-bot")
                 if(channel){
-                    channel.send(message.content)
-                    console.log(message.content)
+                    channel.send(args)
+                    console.log(args)
                 } else{
                     guild.channels.create("maj-patouuu-bot", {
                         type: "text",
                         topic: "Channel ou est noté chaque mise a jour du bot !"
                     }).then(() => {
                         let channel = guild.channels.cache.find(channel => channel.name === "maj-patouuu-bot")
-                        channel.send(message.content)
-                        console.log(message.content)
+                        channel.send(args)
+                        console.log(args)
                     })
                 }
             } catch (err) {
