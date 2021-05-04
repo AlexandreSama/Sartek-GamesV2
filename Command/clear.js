3a6aaba15cf676c856a2cf0461ea309482d11d57
Alexandre <alexandrehainy12@gmail.com>
2021-05-04 18:44:11 +0200

    add then promise

--- Command/clear.js
+++ Command/clear.js
@@ -7,9 +7,9 @@ module.exports.run = (client, message, args) => {
 	if (message.member.hasPermission("ADMINISTRATOR")) {
         let arguments = args.join("")
         if(!arguments){
-            message.channel.send("Tu dois me donner un nombre de messages a supprimer stp !")
+            message.channel.send("Tu dois me donner un nombre de messages a supprimer stp !").then(messsages => messsages.delete())
         }else{
-            message.channel.bulkDelete(arguments).then(messages => message.channel.send(arguments + " messages supprimés"))
+            message.channel.bulkDelete(arguments).then(messages => message.channel.send(arguments + " messages supprimés").then(messsages => messsages.delete()))
         }
     } else {
         message.author.send("Tu n'a pas la permission de faire cet commande !")
