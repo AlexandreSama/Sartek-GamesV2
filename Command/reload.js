2e0822393a40a9a399a878a602452eddad60259e
Alexandre <alexandrehainy12@gmail.com>
2021-05-09 16:46:38 +0200

    test

--- Command/reload.js
+++ Command/reload.js
@@ -6,8 +6,10 @@ module.exports.run = (client, message, args) => {
     message.delete()
 
 	message.channel.send("Reboot du bot en cours....").then(res => {
-        shell.exec("pm2 stop 1", {async: true})
-        shell.exec("pm2 start 1")
+        var child = exec('pm2 stop 1', {async:true});
+        child.stdout.on('data', function(data) {
+            shell.exec("pm2 start 1")
+        });
     })
 } 
 
