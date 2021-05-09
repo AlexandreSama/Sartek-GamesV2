47703ddae6744b3b78a6d57fb3a834ed5b6f3b36
Alexandre <alexandrehainy12@gmail.com>
2021-05-09 16:43:19 +0200

    fix ?

--- Command/reload.js
+++ Command/reload.js
@@ -6,7 +6,7 @@ module.exports.run = (client, message, args) => {
     message.delete()
 
 	message.channel.send("Reboot du bot en cours....").then(res => {
-        shell.exec("pm2 stop 1", {async: true}).then(res2 => {
+        shell.exec("pm2 stop 1", {async: true}, function(code, stdout, stderr){
             shell.exec("pm2 start 1")
         })
     })
