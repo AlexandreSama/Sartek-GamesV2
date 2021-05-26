089815a321440fbfb45cfbf9e526a5dd62ad0d42
Patouuu <serveur.khg.fr@outlook.fr>
2021-05-26 16:10:43 +0200

    maj help ban

--- Command/help.js
+++ Command/help.js
@@ -23,7 +23,7 @@ module.exports.run = (client, message) => {
                     //Copie a partir de cet accolade
                     {
                         name: "**__ban__:**",
-                        value: "Ex : +ban @Patouuu 10d/m raison (d = days , m = minutes choisir l'un ou l'autre)",
+                        value: "Ex : +ban @Patouuu 10d/m raison (d = days , m = minutes choisir l'un ou l'autre , le max de jours de ban que discord accepte est de 6000d (13 ans))",
                         inline: true
                     },
                     //Jusqu'ici et colle juste en dessous de la dernière (ATTENTION ! Pense a mettre une virgule a chaque fin d'accolade !)
