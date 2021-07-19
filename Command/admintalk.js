85fc30cf7295901fec440f11587f0c674d9e78a4
Alexandre <alexandrehainy12@gmail.com>
2021-07-19 23:08:54 +0200

    refactor code

--- Command/admintalk.js
+++ Command/admintalk.js
@@ -6,17 +6,21 @@ module.exports.run = async (client, message, args) => {
 
     const filter = message => message.author.id == message.author.id;
     
-    await message.channel.send("Quel serveur choisissez-vous ?").then(res => {
+    if(message.author.id === "724693796499095552" || message.author.id === "256892994504884224"){
+        await message.channel.send("Quel serveur choisissez-vous ?").then(res => {
             res.channel.awaitMessages(filter, {max: 1}).then(response => {
-            let guild = client.guilds.cache.get(response.first().content)
-            message.channel.send("Que voulez-vous dire au créateur du serveur ?").then(res2 => {
-                res2.channel.awaitMessages(filter, {max: 1}).then(responseAdmin => {
-                    guild.owner.send("**Bonjour/Bonsoir, vous avez reçu un message de la part de mes créateurs ! Voici ce qu'il contient :** \n\n" + responseAdmin.first().content)
-                    message.channel.send("Message envoyé avec succés !")
+                let guild = client.guilds.cache.get(response.first().content)
+                message.channel.send("Que voulez-vous dire au créateur du serveur ?").then(res2 => {
+                    res2.channel.awaitMessages(filter, {max: 1}).then(responseAdmin => {
+                        guild.owner.send("**Bonjour/Bonsoir, vous avez reçu un message de la part de mes créateurs ! Voici ce qu'il contient :** \n\n" + responseAdmin.first().content)
+                        message.channel.send("Message envoyé avec succés !")
+                    })
                 })
             })
         })
-    })
+    }else{
+        message.channel.send("Qu'a tu donc cru pouvoir faire ? :eyes:")
+    }
 }
 
 module.exports.help = {
