3ab358ac9ccfb95e9652f8f1fc2a55974a385e5d
Alexandre <alexandrehainy12@gmail.com>
2021-07-19 23:07:18 +0200

    fix (finally) admintalk

--- Command/admintalk.js
+++ Command/admintalk.js
@@ -4,55 +4,17 @@ module.exports.run = async (client, message, args) => {
 
     message.delete()
 
-    let authorid = message.author.id;
-    const filter = message => message.author.id == authorid;
+    const filter = message => message.author.id == message.author.id;
     
     await message.channel.send("Quel serveur choisissez-vous ?").then(res => {
-        res.channel.awaitMessages(filter, {max: 1}).then(response => {
-            let responseServer = response.first().content;
-            let serverArray = client.guilds.cache.get(responseServer)
-            do {
-                if(!serverArray.channels.cache.find(channel => channel.name === "intra-server")){
-                    serverArray.channels.create("Intra-server", {type: "text", permissionOverwrites: [{
-                        id: serverArray.ownerID,
-                        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
-                    },
-                    {
-                        id: serverArray.roles.everyone,
-                        deny: ['VIEW_CHANNEL', 'SEND_MESSAGES']
-                    }
-                    ]})
-                    message.channel.send("Que voulez-vous répondre a cet personne ?").then(res2 => {
-                        res2.channel.awaitMessages(filter, {max: 1}).then(response2 => {
-                            let sendedMessage = response2.first().content;
-                            let filter2 = message => message.author.id == serverArray.owner.id;
-                            let channelTalkAdmin = serverArray.channels.cache.find(channel => channel.name === "intra-server")
-                            channelTalkAdmin.send("<@" + serverArray.ownerID + ">, vous avez reçu un message de : " + message.author.username + " ! Voici ce qu'il contient : **" + sendedMessage+ "**").then(resIntra => {
-                                message.channel.send("**Message Envoyé !**")
-                                resIntra.channel.awaitMessages(filter2, {max: 1}).then(responseIntra => {
-                                    let resIntras = responseIntra.first().content;
-                                    message.channel.send(serverArray.owner.nickname + " Vous a répondu : **" + resIntras + "**")
-                                })
-                            })
-                        })
-                    })
-                }else{
-                    message.channel.send("Que voulez-vous répondre a cet personne ?").then(res2 => {
-                        res2.channel.awaitMessages(filter, {max: 1}).then(response2 => {
-                            let sendedMessage = response2.first().content;
-                            let filter2 = message => message.author.id == serverArray.owner.id;
-                            let channelTalkAdmin = serverArray.channels.cache.find(channel => channel.name === "intra-server")
-                            channelTalkAdmin.send("<@" + serverArray.ownerID + ">, vous avez reçu un message de : " + message.author.username + " ! Voici ce qu'il contient : **" + sendedMessage+ "**").then(resIntra => {
-                                message.channel.send("**Message Envoyé !**")
-                                resIntra.channel.awaitMessages(filter2, {max: 1}).then(responseIntra => {
-                                    let resIntras = responseIntra.first().content;
-                                    message.channel.send(serverArray.owner.nickname + " Vous a répondu : **" + resIntras + "**")
-                                })
-                            })
-                        })
-                    })
-                }
-            } while (message.content === "fin de transmission");
+            res.channel.awaitMessages(filter, {max: 1}).then(response => {
+            let guild = client.guilds.cache.get(response.first().content)
+            message.channel.send("Que voulez-vous dire au créateur du serveur ?").then(res2 => {
+                res2.channel.awaitMessages(filter, {max: 1}).then(responseAdmin => {
+                    guild.owner.send("**Bonjour/Bonsoir, vous avez reçu un message de la part de mes créateurs ! Voici ce qu'il contient :** \n\n" + responseAdmin.first().content)
+                    message.channel.send("Message envoyé avec succés !")
+                })
+            })
         })
     })
 }
